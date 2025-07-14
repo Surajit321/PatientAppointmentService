afe0ece40182e54b84c22daaf1ca4e067450eed8
Surajit Borah <surajitsb.borah@gmail.com>
2025-07-14 22:50:32 +0530

    Added cors configuration

---
+++ src/main/java/com/patientAppointment/patientAppointment/configurations/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.patientAppointment.patientAppointment.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "https://patient-appointment-netlify.net")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
